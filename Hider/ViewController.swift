37337e8cdd74a49aa3aa39b1dd5fac389706fd1e
João Covas <jmdcovas@MBP-de-Joao-2.lan>
2015-10-27 20:10:16 +0000

    fist commit

--- Hider/ViewController.swift
+++ Hider/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageBlue: UIImageView!
+    @IBOutlet weak var imageRed: UIImageView!
+    @IBOutlet weak var buttonBlue: UIButton!
+    @IBOutlet weak var buttonRed: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+        imageBlue.hidden = true
+    }
+    @IBAction func hideRed(sender: AnyObject) {
+        imageRed.hidden = true
+    }
 
 }
 
